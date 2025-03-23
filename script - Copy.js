174e495b2d46c4392b36f08c667bfe7a19e86363
shivamjai08 <shijai2580@gmail.com>
2025-03-24 00:18:21 +0530

    frist commit

---
+++ script - Copy.js
@@ -0,0 +1,13 @@
+console.log("welcome to my spotify")
+let songs=[
+    {songname:"choliya ke hook raja ji ",filepath:"song/1.mp3",coverpath:"cover/1.jpg"},
+
+    {songname:"choliya ke hook raja ji ",filepath:"song/1.mp3",coverpath:"cover/1.jpg"},
+
+    {songname:"choliya ke hook raja ji ",filepath:"song/1.mp3",coverpath:"cover/1.jpg"},
+
+    {songname:"choliya ke hook raja ji ",filepath:"song/1.mp3",coverpath:"cover/1.jpg"},
+
+    {songname:"choliya ke hook raja ji ",filepath:"song/1.mp3",coverpath:"cover/1.jpg"}
+ ]
+let audioelement=new Audio('1.mp3');
